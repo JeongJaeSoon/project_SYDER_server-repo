bd8992d27c0f33c4c511c58e05a22087f063a1fa
94soon <53788601+JeongJaeSoon@users.noreply.github.com>
2020-05-16 03:35:16 +0900

    set FCM_deviceToken_migration

---
+++ database/migrations/2020_05_16_032552_create_devices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDevicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->foreign('sender')->references('id')->on('users')->onDelete('cascade');
+            $table->string('device_id')->nullable();
+            $table->string('push_service_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devices');
+    }
+}
